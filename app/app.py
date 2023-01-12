524af9da4c415eb28ef66cf2d87688feb4873067
ayberkuckun <44614567+ayberkuckun@users.noreply.github.com>
2023-01-12 15:16:14 +0100

    update button

--- app/app.py
+++ app/app.py
@@ -2,16 +2,21 @@ import gradio as gr
 import hopsworks
 
 project = hopsworks.login()
-fs = project.get_feature_store()
 
 dataset_api = project.get_dataset_api()
 
-dataset_api.download("Resources/aqi/images/df_next_7_days.png")
+dataset_api.download("Resources/aqi/images/df_next_7_days.png", overwrite=True)
+
+
+def update():
+    dataset_api.download("Resources/aqi/images/df_next_7_days.png", overwrite=True)
+    return "df_next_7_days.png"
+
 
 with gr.Blocks() as demo:
     with gr.Row():
         with gr.Column():
-            gr.Label("Paris - AQI Predictions for the Next 7 Days")
-            input_img = gr.Image("df_next_7_days.png", elem_id="predicted-img")
+            gr.Label("Paris - AQI Predictions")
+            gr.Button("Update Predictions").click(update, None, gr.Image("df_next_7_days.png"))
 
 demo.launch()
